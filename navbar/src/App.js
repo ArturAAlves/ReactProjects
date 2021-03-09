627e3ad6f34f43c0a264037e86bd1f56c80990cf
Artur Alves <artudoalves@gmail.com>
2021-03-09 22:09:49 +0000

    sideSlider

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./navbar"
+import SideBar from "./sidebar"
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
+      <SideBar />
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
