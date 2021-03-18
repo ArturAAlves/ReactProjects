1482a85b4702d36b45c4d86971622c6ae6e0f394
Artur Alves <artudoalves@gmail.com>
2021-03-18 23:42:40 +0000

    update

--- movie-app/src/pages/Movies.js
+++ movie-app/src/pages/Movies.js
@@ -10,27 +10,34 @@ import "../scss/App.scss"
 const Trending = () => {
     const [page, setPage] = useState(1)
     const [content, setContent] = useState([])
-    const [totalPages, setTotalPages] = useState(10)
-
+    const [totalPages, setTotalPages] = useState(1)
+    const [selectedGenres, setSelectedGenres] = useState([])
+    const [genres, setGenres] = useState([])
+    const contentType = "movie"
     const fetchMovies = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&page=${page}`)
         setContent(data.results)
         setTotalPages(data.total_pages)
+
     }
     useEffect(() => {
         fetchMovies()
     }, [page])
-
-
-    console.log(totalPages)
-
     return (
         <div className="main-page-container">
             <div className="trending-content">
-                <Genres />
                 <h3 className="page-tittles">Movies</h3>
+                <Genres
+                    type={contentType}
+                    selectedGenres={selectedGenres}
+                    genres={genres}
+                    setGenres={setGenres}
+                    setSelectedGenres={setSelectedGenres}
+                />
+
+
                 {content && content.map((item) => (
-                    <Item item={item} contentType="movie" key={item.id} />
+                    <Item item={item} contentType={contentType} key={item.id} />
                 ))}
                 <PaginationComponent setPage={setPage} totalPages={totalPages} />
             </div>
