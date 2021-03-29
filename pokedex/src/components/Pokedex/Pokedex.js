c9885f66ec1a0af21528160ce757739f062e18be
Artur Alves <artudoalves@gmail.com>
2021-03-29 23:26:54 +0100

    pokedex

---
+++ pokedex/src/components/Pokedex/Pokedex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PokeCard from "../PokeCard/PokeCard"
+
+
+function Pokedex() {
+    return (
+        <div>
+            <PokeCard />
+        </div>
+    )
+}
+
+export default Pokedex
