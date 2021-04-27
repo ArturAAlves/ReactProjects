799eecb4ea6623a5609d309c8389a933dbd5a06e
Artur Alves <artudoalves@gmail.com>
2021-04-27 22:20:41 +0100

    form

--- amazon/src/components/Order/Order.js
+++ amazon/src/components/Order/Order.js
@@ -4,9 +4,10 @@ import { useStateValue } from "../../StateProvider";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "../../reducer";
 import { getTotalProducs } from "../../reducer";
-import { Link } from "react-router-dom";
 import { Radio, TextField } from "@material-ui/core";
-import { Translate } from "@material-ui/icons";
+import paypal from "./img/paypal.png";
+import mastercard from "./img/mastercard.png";
+import visa from "./img/visa.png";
 
 const Order = () => {
 	const orderInfoTemp = [
@@ -24,13 +25,14 @@ const Order = () => {
 	];
 	const [loaded, setLoaded] = useState(false);
 	const [{ user, basket }, dispatch] = useStateValue();
-	const [selectedValue, setSelectedValue] = React.useState("a");
+	const [selectedValue, setSelectedValue] = useState(null);
 	const [orderInfo, setOrderInfo] = useState();
 
 	const handleChange = (event) => {
 		let value = event.target.value;
 		let field = event.target.id;
-		setSelectedValue(event.target.value);
+		console.log(value);
+
 		switch (field) {
 			case "name":
 				setOrderInfo({ ...orderInfo, name: value });
@@ -38,10 +40,47 @@ const Order = () => {
 			case "adress":
 				setOrderInfo({ ...orderInfo, adress: value });
 				break;
+			case "city":
+				setOrderInfo({ ...orderInfo, city: value });
+				break;
+			case "postal":
+				setOrderInfo({ ...orderInfo, postal: value });
+				break;
+			case "phoneNumber":
+				setOrderInfo({ ...orderInfo, phone: value });
+				break;
+			case "typeA":
+				setOrderInfo({ ...orderInfo, method: value });
+				break;
+			case "typeB":
+				setOrderInfo({ ...orderInfo, method: value });
+				break;
+			case "typeC":
+				setOrderInfo({ ...orderInfo, method: value });
+				break;
+			case "cardNumber":
+				setOrderInfo({ ...orderInfo, cardN: value });
+				break;
+			case "year":
+				setOrderInfo({ ...orderInfo, year: value });
+				break;
+			case "securityCode":
+				setOrderInfo({ ...orderInfo, code: value });
+				break;
 			default:
 				break;
 		}
+
+		if (field === "typeC" || field === "typeB" || field === "typeA") {
+			setSelectedValue(value);
+			console.log("hello");
+		}
 	};
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(orderInfo);
+	}
 
 	useEffect(() => {
 		setLoaded(true);
@@ -50,6 +89,13 @@ const Order = () => {
 		}
 		return () => {};
 	}, [loaded]);
+
+	// function validation() {
+	// 	let email = /\S+@\S+\.\S+/.test(email.value)
+	// 		? ""
+	// 		: "Please Enter an Email Email";
+	// 	let mobile = phone.value.length < 9 ? "" : "Please enter 10 numbers";
+	// }
 
 	console.log(orderInfo);
 	return (
@@ -84,23 +130,30 @@ const Order = () => {
 				</div>
 			</div>
 			<div className="order-contact-form">
-				<form>
+				<form onSubmit={handleSubmit}>
 					<TextField
+						required
 						onChange={handleChange}
 						id="name"
 						label="Full Name"
 						placeholder="Full Name"
 						variant="outlined"
+
+						// helperText="Please Enter Your name"
 					/>
+
 					<TextField
+						required
 						onChange={handleChange}
 						id="adress"
 						label="Adress"
 						placeholder="Adress"
 						variant="outlined"
 					/>
+
 					<div className="order-contact-form-sub">
 						<TextField
+							required
 							onChange={handleChange}
 							id="city"
 							label="City"
@@ -108,6 +161,7 @@ const Order = () => {
 							variant="outlined"
 						/>
 						<TextField
+							required
 							onChange={handleChange}
 							id="postal"
 							label="Postal Code"
@@ -117,69 +171,79 @@ const Order = () => {
 					</div>
 
 					<TextField
+						required
 						onChange={handleChange}
 						style={{ width: "225px" }}
-						id="PhoneNumber"
+						id="phoneNumber"
 						label="Phone number"
 						placeholder="Phone number"
 						variant="outlined"
 					/>
 					<h3 style={{ marginTop: "15px" }}>Payment Method</h3>
 					<div className="order-contact-form-payment-method">
-						<Radio
-							checked={selectedValue === "a"}
-							onChange={handleChange}
-							value="a"
-							color="default"
-							name="radio-button-demo"
-							inputProps={{ "aria-label": "A" }}
-							size="small"
-						/>
-						<Radio
-							checked={selectedValue === "b"}
-							onChange={handleChange}
-							value="b"
-							color="default"
-							name="radio-button-demo"
-							inputProps={{ "aria-label": "B" }}
-							size="small"
-						/>
-						<Radio
-							checked={selectedValue === "c"}
-							onChange={handleChange}
-							value="c"
-							color="default"
-							name="radio-button-demo"
-							inputProps={{ "aria-label": "C" }}
-							size="small"
-						/>
-						<Radio
-							checked={selectedValue === "d"}
-							onChange={handleChange}
-							value="d"
-							color="default"
-							name="radio-button-demo"
-							inputProps={{ "aria-label": "D" }}
-							size="small"
-						/>
+						<div className="payment-method">
+							<Radio
+								required
+								id="typeA"
+								onChange={handleChange}
+								checked={selectedValue === "MasterCard"}
+								value="MasterCard"
+								color="default"
+								name="radio-button-demo"
+								inputProps={{ "aria-label": "MasterCard" }}
+								size="small"
+							/>
+							<img src={mastercard} alt="mastercard" />
+						</div>
+						<div className="payment-method">
+							<Radio
+								required
+								id="typeB"
+								onChange={handleChange}
+								checked={selectedValue === "VISA"}
+								value="VISA"
+								color="default"
+								name="radio-button-demo"
+								inputProps={{ "aria-label": "VISA" }}
+								size="small"
+							/>
+							<img src={visa} alt="visa" />
+						</div>
+						<div className="payment-method">
+							<Radio
+								required
+								id="typeC"
+								checked={selectedValue === "Paypal"}
+								onChange={handleChange}
+								value="Paypal"
+								color="default"
+								name="radio-button-demo"
+								inputProps={{ "aria-label": "Paypal" }}
+								size="small"
+							/>
+							<img src={paypal} alt="paypal" />
+						</div>
 					</div>
 					<div className="order-contact-form-payment">
 						<TextField
-							onChange={handleChange}
+							required
 							id="cardNumber"
+							onChange={handleChange}
 							label="Card Number"
 							placeholder="Card Number"
 							variant="outlined"
 						/>
 
 						<TextField
-							onChange={handleChange}
+							required
 							id="year"
+							onChange={handleChange}
 							label="Year"
 							placeholder="Year"
 							variant="outlined"
 						/>
 						<TextField
+							required
 							onChange={handleChange}
 							id="securityCode"
 							label="Code"
@@ -187,41 +251,38 @@ const Order = () => {
 							variant="outlined"
 						/>
 					</div>
-				</form>
-			</div>
-
-			<div className="subtotal-Order">
-				<div className="subtotal-text">
-					<p>
-						Total
-						<span>
-							{" "}
-							({basket.length !== 0 ? getTotalProducs(basket) : 0} items) :{" "}
-						</span>
-						<CurrencyFormat
-							fixedDecimalScale={true}
-							value={basket.length !== 0 ? getBasketTotal(basket) : 0}
-							decimalScale={2}
-							displayType={"text"}
-							thousandSeparator={true}
-							prefix={"€"}
-							renderText={(value) => (
-								<span className="subtotal-value">{value}</span>
-							)}
-						/>
-					</p>
-					<div className="subtotal-checkout-offer">
-						<input type="checkbox" />
-						<p>This order contains a Gift</p>
+					<div className="subtotal-Order">
+						<div className="subtotal-text">
+							<p>
+								Total
+								<span>
+									{" "}
+									({basket.length !== 0 ? getTotalProducs(basket) : 0} items) :{" "}
+								</span>
+								<CurrencyFormat
+									fixedDecimalScale={true}
+									value={basket.length !== 0 ? getBasketTotal(basket) : 0}
+									decimalScale={2}
+									displayType={"text"}
+									thousandSeparator={true}
+									prefix={"€"}
+									renderText={(value) => (
+										<span className="subtotal-value">{value}</span>
+									)}
+								/>
+							</p>
+							<div className="subtotal-checkout-offer">
+								<input type="checkbox" />
+								<p>This order contains a Gift</p>
+							</div>
+						</div>
+						<div className="subtotal-checkout-btn-order">
+							<button className="checkout-btn" type="submit" value="Submit">
+								Comple Purchase
+							</button>
+						</div>
 					</div>
-				</div>
-				<div className="subtotal-checkout-btn-order">
-					<Link to="./order">
-						<button className="checkout-btn" type="button">
-							Comple Purchase
-						</button>
-					</Link>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
