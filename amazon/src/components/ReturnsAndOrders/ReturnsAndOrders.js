b5cb18dc57b3fc00148507a5179f22c688e0bc55
Artur Alves <artudoalves@gmail.com>
2021-05-04 13:59:25 +0100

    update

--- amazon/src/components/ReturnsAndOrders/ReturnsAndOrders.js
+++ amazon/src/components/ReturnsAndOrders/ReturnsAndOrders.js
@@ -3,6 +3,7 @@ import { useStateValue } from "../../StateProvider";
 import Paypal from "./img/paypal.png";
 import MasterCard from "./img/mastercard.png";
 import Visa from "./img/visa.png";
+import "./ReturnsAndOrders.scss";
 
 const ReturnsAndOrders = () => {
 	const [{ purchase, contats }, dispatch] = useStateValue(); // eslint-disable-line
@@ -24,66 +25,107 @@ const ReturnsAndOrders = () => {
 	}
 	if (purchase[0]) {
 		let { basket, purchaseID, address, presentNote } = purchase[0];
-		console.log(basket, purchaseID, address, presentNote);
+		console.log(purchase);
 	}
 
-	return (
-		<div className="order">
-			{/* <div className="checkout-Order">
-				<div className="checkout-banner">
-					<div className="checkout-banner-img">
-						<img
-							alt="card-img"
-							src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/OCC_146VC._CB485945274_.jpg"
-						/>
-					</div>
-					<div className="checkout-banner-text">
-						<p>
-							Pre-Approval with a YES or NO in <strong>60 seconds </strong>for
-							Ocean Credit Card get up to <strong>€1,500 credit.</strong>
-						</p>
-					</div>
-					<div className="checkout-banner-btn">
-						<img
-							src="https://images-na.ssl-images-amazon.com/images/G/02/creditcard/Learn_More._CB485946358_.gif"
-							alt="btn"
-						/>
-					</div>
-				</div>
-				<div className="personal-info">
-					<div>
-						<span style={{ textDecoration: "underline" }}>
-							{user ? user.email : ""}
-						</span>{" "}
-					</div>
-				</div>
-			</div> */}
+	const timer = (countDownDate) => {
+		let newDate = parseInt(countDownDate);
+		var now = new Date().getTime();
+		var timeleft = newDate - now;
+		var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+		var hours = Math.floor(
+			(timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+		console.log(timeleft);
+	};
 
-			<div className="order-contact-form">
-				{/* <div className="contact-info">
-					{contacts.map((contact) => (
-						<div>
-							<div className="adress">
-								<h3>Adress:</h3>
-								<p>Full name: {contact.name}</p>
-								<p>Adress: {contact.adress}</p>
-								<p>City: {contact.city}</p>
-								<p>Postal Code: {contact.postal}</p>
-								<p>Phone Number: {contact.phone}</p>
-							</div>
-							<div className="payment">
-								<h3>Payment Info</h3>
-								<div className="payment-type">
-									<img src={imgSelector(contact.method)} alt={contact.method} />
-								</div>
+	return (
+		<div className="orders-and-returns">
+			<div className="orders-container">
+				{purchase.map((order) => (
+					<div className="order-container" key={order.purchaseID}>
+						<div className="order-info">
+							<div className="order-id">
 								<p>
-									Card Number :{contact.cardN} / year :{contact.year} / Code :
-									{contact.code}
+									Order ID: <span>{order.purchaseID}</span>
 								</p>
 							</div>
+							<div className="order-date">
+								<p className="order-tittle">Order Date</p>
+								{/* <p>{order.date}</p> */}
+								<p>04 de abril de 2021</p>
+							</div>
+							{order.date ? timer(order.date) : ""}
+							<div className="order-value">
+								<p className="order-tittle">Value</p>
+								<p>€450</p>
+							</div>
+
+							<div className="order-contact-info">
+								<p className="order-tittle">Send To</p>
+								<p>{order.name}</p>
+								<p>{order.address.address}</p>
+								<p>
+									{order.address.city}, {order.address.postal}
+								</p>
+
+								<p>{order.address.phone}</p>
+							</div>
+							<div className="order-payment-info">
+								<div className="order-payment-tittle order-tittle">
+									Payment Info
+								</div>
+								<div className="order-payment-type">
+									<img
+										src={imgSelector(order.address.method)}
+										alt={order.address.method}
+									/>
+								</div>
+								<ul>
+									<li>
+										<span>Card:</span> {order.address.cardN}
+									</li>
+									<li>
+										<span>Year:</span> {order.address.year}
+									</li>
+									<li>
+										<span>Code:</span> {order.address.code}
+									</li>
+								</ul>
+							</div>
 						</div>
-					))}
-				</div> */}
+
+						<div className="order-product-info">
+							<div className="order-item-list">
+								{order.basket.map((item, i) => (
+									<div className="order-item" key={i}>
+										<div className="order-item-info-container">
+											<div className="order-item-img">
+												<img src={item.image} alt={item.title} />
+											</div>
+											<div className="order-item-info">
+												<p className="order-item-info-tittle">{item.title}</p>
+												<p>Price: {item.price}</p>
+												<p>Qty: {item.qty}</p>
+											</div>
+										</div>
+										<div className="order-item-division"></div>
+									</div>
+								))}
+							</div>
+
+							{order.presentNote ? (
+								<div className="order-note">
+									<p>{order.presentNote}</p>
+								</div>
+							) : (
+								""
+							)}
+						</div>
+					</div>
+				))}
 			</div>
 		</div>
 	);
